2bb1a702b405ddc97ee092d771f9b7cb6b8abb3e
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-02-26 20:25:58 +0200

    add apartment controller for handling apartment-related requests

---
+++ server/src/controllers/apartment.controller.ts
@@ -0,0 +1,24 @@
+import asyncHandler from 'express-async-handler';
+import { apartmentService } from '../services';
+import { CREATED, OK } from '../utils';
+import { apartmentSchema, paramsSchema } from '../utils/validations';
+
+export const createApartmentHandler = asyncHandler(async (req, res) => {
+  const parsedApartmentData = apartmentSchema.parse(req.body);
+  const apartment = await apartmentService.createApartment(parsedApartmentData);
+
+  res.status(CREATED).json({ data: apartment });
+});
+
+export const getApartmentsHandler = asyncHandler(async (req, res) => {
+  const apartments = await apartmentService.getApartments();
+
+  res.status(OK).json({ data: apartments });
+});
+
+export const getApartmentDetailsHandler = asyncHandler(async (req, res) => {
+  const { id } = paramsSchema.parse(req.params);
+  const apartment = await apartmentService.getApartmentDetails(id);
+
+  res.status(OK).json({ data: apartment });
+});
