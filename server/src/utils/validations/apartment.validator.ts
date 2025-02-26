0d983505d3a94b18431b7d99d769fcf3be472594
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-02-26 20:35:25 +0200

    enforce lowercase for name and project fields

--- server/src/utils/validations/apartment.validator.ts
+++ server/src/utils/validations/apartment.validator.ts
@@ -1,9 +1,15 @@
 import { z } from 'zod';
 
 export const apartmentSchema = z.object({
-  name: z.string().min(3, 'Name must be at least 3 characters long'),
+  name: z
+    .string()
+    .min(3, 'Name must be at least 3 characters long')
+    .toLowerCase(),
   unitNumber: z.string().min(1, 'Unit number is required'),
-  project: z.string().min(3, 'Project name must be at least 3 characters long'),
+  project: z
+    .string()
+    .min(3, 'Project name must be at least 3 characters long')
+    .toLowerCase(),
   price: z.number().int().positive('Price must be a positive integer'),
   location: z.string().min(3, 'Location must be at least 3 characters long'),
   description: z
